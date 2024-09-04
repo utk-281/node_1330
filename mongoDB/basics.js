e6453e43d73a27a700d3c9e5877efaa1359dcf4e
utkarsh <utkarshgupta.281@gmail.com>
2024-09-04 14:30:03 +0530

    add mongoDB basics definition

---
+++ mongoDB/basics.js
@@ -0,0 +1,50 @@
+//? scaling ==> it refers to upgrading/downgrading the specifications
+
+//! difference between sql and no sql databases
+
+//? sql ==>
+// it is also known as relational databases
+// it stores data in tables i.e, rows and columns
+// it is suitable for handling complex/ nested queries.
+// it supports vertical scaling ==> adding new resources to the existing system
+// it has a predefined schema/structure
+
+//? no sql ==>
+// it stands for not only sql
+// it is also known as non relational databases.
+// it stores data in the form of document, key-value pair, graph, wide column.
+//// ? document based database ==> it stores data in form of JSON/BSON. example==> mongoDB, couchDB
+//// ? key-value pair ==> data is stored in form of keys and values. examples ==> amazon Dynamo DB
+//// ? graph databases ==> data is stored in nodes . example ==> neo4j database
+//// ? wide column db ==> data is stored in dynamic tables ==> cassandraDB
+
+// it is not suitable for handling complex queries.
+// it supports horizontal scaling ==> buying new resources with the existing system.
+// by default, it has dynamic schema/structure.
+
+//? database ==> it is used to store,manage,and retrieve the data which is done by DBMS(database management system)
+
+//? collection ==> multiple documents grouped together is known as a collection
+
+//? documents ==> it is a javascript object which stores data in key value pair
+
+/* structure of document
+
+  {     
+    key1: value1, ==> on field
+    key2: value2,
+    .
+    .
+    .
+    .
+  }
+
+  */
+
+//? what is noSQL databases? ==> it is also known as non relational databases which stores data in document, key-value, graphs, column oriented format.
+
+//? mongoDB ==> it is open source, cross-platform, document oriented database which stores data in JSON/BSON format and has dynamic schema.
+
+//! download mongoDB compass ==> https://www.mongodb.com/try/download/community
+
+//! download mongoDB shell ==> https://www.mongodb.com/try/download/shell
