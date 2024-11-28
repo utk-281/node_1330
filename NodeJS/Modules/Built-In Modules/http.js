041d581f65c1c929ba8a28a571ee7d4eae86213b
utkarsh <utkarshgupta.281@gmail.com>
2024-11-28 15:58:41 +0530

    add http part-1

--- NodeJS/Modules/Built-In Modules/http.js
+++ NodeJS/Modules/Built-In Modules/http.js
@@ -21,11 +21,64 @@
 // 4) assign a port number to our server using listen()
 // 5) to access the server, we have to use http://localhost:port_number
 
-let http = require("http");
+// let http = require("http");
 // console.log(http);
 
+// let server = http.createServer((req, res) => {
+// res.write("this is passed from write()");
+// res.end();
+
+// res.end("hi from server");
+
+// console.log(req);
+// console.log(res);
+
+// res.writeHead(200, "ok", { "Content-Type": "text/plain" });
+// res.write("hello world");
+
+// res.writeHead(200, "ok", { "Content-Type": "text/plain" });
+// res.write(`<h1> hello world </h1>`);
+//? output will be normal string message
+
+// res.writeHead(200, "ok", { "Content-Type": "text/html" });
+// res.write(`<h1> hello world </h1>`);
+
+// res.end();
+// });
+
+// server.listen(9000, (err) => {
+//   if (err) console.log(err);
+//   console.log("server running at http://localhost:9000");
+// });
+
+//! res.end() is used to display the contents on the UI and also terminate the current request-response cycle.
+
+//! write() is only used to display the contents on the UI.
+
+//! writeHead(statusCode, statusMessage, {Content-Type} ) is used to set the headers.
+// statusCode ==> there are total 5 series of status codes
+// =========== 1) 100 ==> Informational      102 ==> processing
+// =========== 2) 200 ==> Successful         200 ==> ok, 201 ==> ok and created, 202 ==> accepted
+// =========== 3) 300 ==> Redirection        301 ==> permanently moved, 302 ==> found
+// =========== 4) 400 ==> Client Side Error  400 ==> bad request, 401 ==> unauthorized, 403 ==> forbidden, 404 ==> not  found
+// =========== 5) 500 ==> Server Side Error  500 ==> internal server error
+
+// { "Content-Type": "text/plain" }       ==> for string contents
+// { "Content-Type": "text/html" }        ==> for html response
+// { "Content-Type": "text/css" }         ==> for css response
+// { "Content-Type": "application/json" } ==> for json response
+
+const http = require("http");
+const fs = require("fs");
+
 let server = http.createServer((req, res) => {
-  //   res.end("hi from server");
+  //! === ===================sending html file======================== ===
+  let data = fs.readFileSync("./index.html", "utf-8");
+  res.writeHead(200, "ok", { "Content-Type": "text/html" });
+  res.end(data);
 });
 
-server.listen(9000);
+server.listen(9000, (err) => {
+  if (err) console.log(err);
+  console.log("server running at http://localhost:9000");
+});
